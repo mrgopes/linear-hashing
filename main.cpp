71b0178bde39f78905178b712deb412131f27451
Michal Blava <michal.blava@gmail.com>
2024-05-07 11:35:07 +0200

    durchaus funktionierendes einfuegen

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include "ADS_set.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+  std::cout << "Hello, World!" << std::endl;
+
+  ADS_set<int> ht{};
+
+  ht.insert(5);
+  ht.insert(2);
+  ht.insert(4);
+  ht.insert(8);
+  ht.insert(11);
+  ht.insert(12);
+  ht.insert(15);
+  ht.insert(20);
+
+  std::cout << "Tschuess!" << std::endl;
+
+  return 0;
 }
