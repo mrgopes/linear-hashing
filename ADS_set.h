992a926674f8aabfc98ccd097376db43e7874f10
Michal Blava <michal.blava@gmail.com>
2024-05-07 11:35:07 +0200

    durchaus funktionierendes einfuegen

--- ADS_set.h
+++ ADS_set.h
@@ -21,89 +21,144 @@ public:
   using key_equal = std::equal_to<key_type>;                       // Hashing
   using hasher = std::hash<key_type>;                              // Hashing
 
+  void insert(std::initializer_list<key_type> ilist) {
+    for (const auto& i : ilist) {
+      insert(i);
+    }
+  }
+
   class Bucket {
   public:
     Key inhalt[N];
-    static size_t id;
+    Bucket* ueberlauf;
+    ADS_set* parent;
 
-    Bucket(): inhalt{} {}
+    explicit Bucket(ADS_set* parent = nullptr): parent{parent}, inhalt{}, ueberlauf{nullptr} {}
+
+    void set_parent(ADS_set* prnt) {
+      parent = prnt;
+    }
 
     Bucket(std::initializer_list<key_type> ilist): inhalt{} {
-      for (size_t i {0}; i < N && i < sizeof(ilist); i++) {
-        inhalt[i] = ilist[i];
+      for (const auto& i : ilist) {
+        insert(i);
+      }
+    }
+
+    void insert(Key item, bool allow_split = true) {
+      if (item == 0) return;
+      if (!full()) {
+        for (size_t i {0}; i < N; i++) {
+          if (!(inhalt[i])) {
+            std::cout << "Das item " << item << " wird eingefuegt zum Platz " << i << std::endl;
+            inhalt[i] = item;
+            std::cout << "Tada: " << inhalt[i] << std::endl;
+            break;
+          }
+        }
+      } else {
+        std::cout << "full" << "\n";
+        if (!ueberlauf) ueberlauf = new Bucket(nullptr);
+        ueberlauf->insert(item);
+        if (allow_split) parent->global_split();
       }
     }
 
     bool full() {
-      return sizeof(inhalt) / sizeof(key_type) == N;
+      for (size_t i {0}; i < N; i++) {
+        if (!(inhalt[i])) {
+          return false;
+        }
+      }
+      return true;
+    }
+
+    void split() {
+      if (!parent) return;
+      for (size_t i {0}; i < N; i++) {
+        if (get_hash_wert(inhalt[i], parent->d + 1) != get_hash_wert(inhalt[i], parent->d)) {
+          parent->inhalt[get_hash_wert(inhalt[i], parent->d+1)].insert(inhalt[i], false);
+          inhalt[i] = {};
+        }
+      }
+      Bucket* ueb {ueberlauf};
+      ueberlauf = nullptr;
+      while (ueb != nullptr) {
+        for (size_t i {0}; i < N; i++) {
+          if (ueb->inhalt[i] == 0) continue;
+          if (get_hash_wert(ueb->inhalt[i], parent->d + 1) != get_hash_wert(ueb->inhalt[i], parent->d)) {
+            parent->inhalt[get_hash_wert(ueb->inhalt[i], parent->d+1)].insert(ueb->inhalt[i], false);
+            ueb->inhalt[i] = {};
+          } else {
+            this->insert(ueb->inhalt[i], false);
+          }
+        }
+        ueb = ueb->ueberlauf;
+      }
+      parent->nextToSplit++;
+      if (parent->nextToSplit == pow(2, parent->d)) {
+        parent->split_weiter();
+      }
     }
   };
 
-  class Knoten {
-  public:
-    Knoten* eltern;
-    bool rechts;
-    Knoten* left;
-    Knoten* right;
-    Bucket* bucket;
+  size_t d;
+  size_t max_sz;
+  Bucket* inhalt;
+  size_t nextToSplit;
 
-    explicit Knoten(bool init = false, Knoten* eltern = nullptr, bool rechts = false): left{nullptr}, right{nullptr}, bucket{nullptr}, eltern{eltern},
-    rechts{rechts} {
-      if (init) {
-        bucket = new Bucket();
-      }
+  ADS_set(): d{0}, max_sz{2}, inhalt{new Bucket[2]}, nextToSplit{0} {
+    inhalt->set_parent(this);
+    inhalt[1].set_parent(this);
+  }
+
+  void global_split() {
+    inhalt[nextToSplit].split();
+    std::cout << "Splitting " << nextToSplit << std::endl;
+  }
+
+  void split_weiter() {
+    nextToSplit = 0;
+    d++;
+    max_sz = pow(2, d + 1);
+
+    Bucket* backup = inhalt;
+    inhalt = new Bucket[pow(2, d + 1)];
+    for (int i {0}; i < pow(2, d); ++i) {
+      inhalt[i] = backup[i];
     }
-
-    Bucket* generiere_unterknoten(bool init = false) {
-      Bucket* backup {bucket};
-      bucket = nullptr;
-      left = new Knoten(init, *this, false);
-      right = new Knoten(init, *this, true);
-      return backup;
+    for (int i {pow(2, d)}; i < pow(2, d+1); ++i) {
+      inhalt[i].set_parent(this);
     }
-
-    void insert(value_type item) {
-      if (bucket) {
-        if (bucket->full()) {
-          // .. split
-        } else {
-          // .. insert
-        }
-      } else {
-        // ... logik ob das n-te bit 1 oder 0 ist
-        if (true) {
-          left->insert(item);
-        }
-      }
-
-    }
-  };
-
-  Knoten wurzel;
-
-  ADS_set(): wurzel{false} {
-    wurzel.generiere_unterknoten(true);
   }
 
   ADS_set(std::initializer_list<key_type> ilist){
-    for (const auto& i : ilist) {
+    insert(ilist);
+  }
 
-    }
+  static int get_hash_wert(const key_type& item, size_t d) {
+    return hasher{}(item) & ((1 << d) - 1);
   }
 
   template<typename InputIt> ADS_set(InputIt first, InputIt last);     // PH1
   ADS_set(const ADS_set &other);
 
-  ~ADS_set();
+  ~ADS_set() {}
 
   ADS_set &operator=(const ADS_set &other);
   ADS_set &operator=(std::initializer_list<key_type> ilist);
 
-  size_type size() const;                                              // PH1
+  size_type size() const {
+    return max_sz;
+  }                                              // PH1
   bool empty() const;                                                  // PH1
 
-  void insert(std::initializer_list<key_type> ilist);                  // PH1
-  std::pair<iterator,bool> insert(const key_type &key);
+  std::pair<iterator,bool> insert(const key_type &key) {
+    size_t wert {get_hash_wert(key, d)};
+    if (wert < nextToSplit) inhalt[get_hash_wert(key, d + 1)].insert(key);
+    else inhalt[wert].insert(key);
+  }
+
   template<typename InputIt> void insert(InputIt first, InputIt last); // PH1
 
   void clear();
@@ -121,6 +176,14 @@ public:
 
   friend bool operator==(const ADS_set &lhs, const ADS_set &rhs);
   friend bool operator!=(const ADS_set &lhs, const ADS_set &rhs);
+
+  static size_t pow(size_t lop, size_t rop) {
+    if (rop == 0) return 1;
+    for (int i {0}; i < rop - 1; ++i) {
+      lop *= lop;
+    }
+    return lop;
+  }
 };
 
 template <typename Key, size_t N>
@@ -144,7 +207,7 @@ public:
 template <typename Key, size_t N>
 void swap(ADS_set<Key,N> &lhs, ADS_set<Key,N> &rhs) { lhs.swap(rhs); }
 
-template <typename Key, size_t N>
-size_t ADS_set<Key, N>::Bucket::id = 0;
+//template <typename Key, size_t N>
+//size_t ADS_set<Key, N>::d = 1;
 
 #endif // ADS_SET_H
