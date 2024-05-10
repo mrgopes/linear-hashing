35d7625f73d5dd449967bc704a6e574d57257e6c
Michal Blava <michal.blava@gmail.com>
2024-05-10 15:46:30 +0200

    btest läuft

--- ADS_set.h
+++ ADS_set.h
@@ -9,37 +9,57 @@
 template <typename Key, size_t N = 3/* default N (implementation-defined) */>
 class ADS_set {
 public:
-  class ForwardIterator /* iterator type (implementation-defined) */;
+  //class ForwardIterator /* iterator type (implementation-defined) */;
   using value_type = Key;
   using key_type = Key;
   using reference = value_type &;
   using const_reference = const value_type &;
   using size_type = size_t;
   using difference_type = std::ptrdiff_t;
-  using const_iterator = const ForwardIterator/* iterator type */;
-  using iterator = const_iterator;
+  //using const_iterator = const ForwardIterator/* iterator type */;
+  //using iterator = const_iterator;
   using key_equal = std::equal_to<key_type>;                       // Hashing
   using hasher = std::hash<key_type>;                              // Hashing
 
   void insert(std::initializer_list<key_type> ilist) {
-    for (const auto& i : ilist) {
-      insert(i);
-    }
+    insert(ilist.begin(), ilist.end());
   }
 
   class Bucket {
   public:
-    Key inhalt[N];
+    Key* inhalt[N];
     Bucket* ueberlauf;
     ADS_set* parent;
 
-    explicit Bucket(ADS_set* parent = nullptr): parent{parent}, inhalt{}, ueberlauf{nullptr} {}
+    size_t sz;
+
+    Bucket(ADS_set* parent = nullptr): inhalt{new Key[N]}, ueberlauf{nullptr}, parent{parent} {
+      for (size_t i {N}; i < N; ++i) {
+        inhalt[i] = nullptr;
+      }
+    }
+
+    bool find(key_type key) const {
+      for (size_t i {0}; i < N; i++) {
+        if (inhalt[i] && key_equal{}(*(inhalt[i]), key)) return true;
+      }
+      if (ueberlauf != nullptr) {
+        Bucket* ueb {ueberlauf};
+        while (ueb != nullptr) {
+          for (size_t j {0}; j < N; j++) {
+            if (ueb->inhalt[j] && key_equal{}(*(ueb->inhalt[j]), key)) return true;
+          }
+          ueb = ueb->ueberlauf;
+        }
+      }
+      return false;
+    }
 
     void set_parent(ADS_set* prnt) {
       parent = prnt;
     }
 
-    Bucket(std::initializer_list<key_type> ilist): inhalt{} {
+    Bucket(std::initializer_list<key_type> ilist): Bucket() {
       for (const auto& i : ilist) {
         insert(i);
       }
@@ -48,14 +68,15 @@ public:
     bool insert(Key item, bool allow_split = true) {
       if (!full()) {
         for (size_t i {0}; i < N; i++) {
-          if (inhalt[i] == item) return false;
+          if (inhalt[i] && key_equal{}(*(inhalt[i]), item)) return false;
         }
         for (size_t i {0}; i < N; i++) {
           if (!(inhalt[i])) {
-            inhalt[i] = item;
+            inhalt[i] = new Key{item};
             return true;
           }
         }
+        return false;
       } else {
         if (!ueberlauf) ueberlauf = new Bucket(nullptr);
         bool result = ueberlauf->insert(item);
@@ -66,7 +87,7 @@ public:
 
     bool full() {
       for (size_t i {0}; i < N; i++) {
-        if (!(inhalt[i])) {
+        if (inhalt[i] == nullptr) {
           return false;
         }
       }
@@ -76,9 +97,9 @@ public:
     void split() {
       if (!parent) return;
       for (size_t i {0}; i < N; i++) {
-        if (get_hash_wert(inhalt[i], parent->d + 1) != get_hash_wert(inhalt[i], parent->d)) {
-          parent->inhalt[get_hash_wert(inhalt[i], parent->d+1)].insert(inhalt[i], false);
-          inhalt[i] = {};
+        if (get_hash_wert(*(inhalt[i]), parent->d + 1) != get_hash_wert(*(inhalt[i]), parent->d)) {
+          parent->inhalt[get_hash_wert(*(inhalt[i]), parent->d+1)].insert(*(inhalt[i]), false);
+          *(inhalt[i]) = {};
         }
       }
       Bucket* ueb {ueberlauf};
@@ -86,11 +107,11 @@ public:
       while (ueb != nullptr) {
         for (size_t i {0}; i < N; i++) {
           if (ueb->inhalt[i] == 0) continue;
-          if (get_hash_wert(ueb->inhalt[i], parent->d + 1) != get_hash_wert(ueb->inhalt[i], parent->d)) {
-            parent->inhalt[get_hash_wert(ueb->inhalt[i], parent->d+1)].insert(ueb->inhalt[i], false);
-            ueb->inhalt[i] = {};
+          if (get_hash_wert(*(ueb->inhalt[i]), parent->d + 1) != get_hash_wert(*(ueb->inhalt[i]), parent->d)) {
+            parent->inhalt[get_hash_wert(*(ueb->inhalt[i]), parent->d+1)].insert(*(ueb->inhalt[i]), false);
+            *(ueb->inhalt[i]) = {};
           } else {
-            this->insert(ueb->inhalt[i], false);
+            this->insert(*(ueb->inhalt[i]), false);
           }
         }
         ueb = ueb->ueberlauf;
@@ -124,29 +145,32 @@ public:
 
     Bucket* backup = inhalt;
     inhalt = new Bucket[pow(2, d + 1)];
-    for (int i {0}; i < pow(2, d); ++i) {
+    for (size_t i {0}; i < pow(2, d); ++i) {
       inhalt[i] = backup[i];
     }
-    for (int i {pow(2, d)}; i < pow(2, d+1); ++i) {
+    for (size_t i {pow(2, d)}; i < pow(2, d+1); ++i) {
       inhalt[i].set_parent(this);
     }
   }
 
-  ADS_set(std::initializer_list<key_type> ilist){
-    insert(ilist);
+  ADS_set(std::initializer_list<key_type> ilist): ADS_set() {
+    insert(ilist.begin(), ilist.end());
   }
 
-  static int get_hash_wert(const key_type& item, size_t d) {
+  static size_t get_hash_wert(const key_type& item, size_t d) {
     return hasher{}(item) & ((1 << d) - 1);
   }
 
-  template<typename InputIt> ADS_set(InputIt first, InputIt last);     // PH1
-  ADS_set(const ADS_set &other);
+  template<typename InputIt> ADS_set(InputIt first, InputIt last) : ADS_set() {
+    insert(first, last);
+  }
 
-  ~ADS_set() {}
+  //ADS_set(const ADS_set &other);
 
-  ADS_set &operator=(const ADS_set &other);
-  ADS_set &operator=(std::initializer_list<key_type> ilist);
+  //~ADS_set() {}
+
+  //ADS_set &operator=(const ADS_set &other);
+  //ADS_set &operator=(std::initializer_list<key_type> ilist);
 
   size_type size() const {
     return sz;
@@ -155,43 +179,56 @@ public:
     return sz == 0;
   }
 
+  //PH1 todo
+  /*
   std::pair<iterator,bool> insert(const key_type &key) {
     size_t wert {get_hash_wert(key, d)};
     if (wert < nextToSplit) sz += inhalt[get_hash_wert(key, d + 1)].insert(key);
     else sz += inhalt[wert].insert(key);
-  }
+  }*/
 
   template<typename InputIt> void insert(InputIt first, InputIt last) {
     for (auto it {first}; it != last; ++it) {
-      insert(*it);
+      size_t wert {get_hash_wert(*it, d)};
+      if (wert < nextToSplit) sz += inhalt[get_hash_wert(*it, d + 1)].insert(*it);
+      else sz += inhalt[wert].insert(*it);
     }
   }
 
-  void clear();
-  size_type erase(const key_type &key);
+  //void clear();
+  //size_type erase(const key_type &key);
 
-  size_type count(const key_type &key) const;                          // PH1
-  iterator find(const key_type &key) const;
+  size_type count(const key_type &key) const {
+    size_t wert {get_hash_wert(key, d)};
+    if (wert < nextToSplit) return inhalt[get_hash_wert(key, d + 1)].find((key));
+    else return inhalt[wert].find((key));
+  }
+  //iterator find(const key_type &key) const;
 
-  void swap(ADS_set &other);
+  //void swap(ADS_set &other);
 
-  const_iterator begin() const;
-  const_iterator end() const;
+  //const_iterator begin() const;
+  //const_iterator end() const;
 
-  void dump(std::ostream &o = std::cerr) const;
+  void dump(std::ostream &o = std::cerr) const {
+    o << "hi" << std::endl;
+  }
 
+  /*
   friend bool operator==(const ADS_set &lhs, const ADS_set &rhs);
   friend bool operator!=(const ADS_set &lhs, const ADS_set &rhs);
+   */
 
   static size_t pow(size_t lop, size_t rop) {
     if (rop == 0) return 1;
-    for (int i {0}; i < rop - 1; ++i) {
+    for (size_t i {0}; i < rop - 1; ++i) {
       lop *= lop;
     }
     return lop;
   }
 };
 
+/*
 template <typename Key, size_t N>
 class ADS_set<Key,N>::ForwardIterator {
 public:
@@ -201,14 +238,14 @@ public:
   using pointer = const value_type *;
   using iterator_category = std::forward_iterator_tag;
 
-  explicit ForwardIterator(/* implementation-dependent */);
+  explicit ForwardIterator(*//* implementation-dependent *//*);
   reference operator*() const;
   pointer operator->() const;
   ForwardIterator &operator++();
   ForwardIterator operator++(int);
   friend bool operator==(const ForwardIterator &lhs, const ForwardIterator &rhs);
   friend bool operator!=(const ForwardIterator &lhs, const ForwardIterator &rhs);
-};
+};*/
 
 template <typename Key, size_t N>
 void swap(ADS_set<Key,N> &lhs, ADS_set<Key,N> &rhs) { lhs.swap(rhs); }
--- main.cpp
+++ main.cpp
@@ -10,22 +10,7 @@ int main() {
 
   std::cout << ht.empty() << std::endl;
 
-  ht.insert(5);
-  ht.insert(5);
-  ht.insert(5);
   ht.insert(v1.begin(), v1.end());
-  ht.insert(11);
-  ht.insert(12);
-  ht.insert(15);
-  ht.insert(20);
-
-  ADS_set<double> sht{};
-
-  sht.insert(1.2);
-  sht.insert(1.2);
-  sht.insert(100029.1);
-  sht.insert(1);
-  sht.insert(998);
 
 
 
