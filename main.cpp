32a218ad2a95c053634dfa2e809807c35fdff2a2
Michal Blava <michal.blava@gmail.com>
2024-05-10 17:20:52 +0200

    OK

--- main.cpp
+++ main.cpp
@@ -4,18 +4,15 @@
 int main() {
   std::cout << "Hello, World!" << std::endl;
 
-  std::vector<int> v1 {8, 12, 4, 5, 2, 11, 15, 20};
+  std::vector<int> v1 {51, 47, 27, 7, 15, 31, 55};
 
   ADS_set<int> ht{};
 
-  std::cout << ht.empty() << std::endl;
+  std::cout << "Vector size: " << v1.size() << std::endl;
 
   ht.insert(v1.begin(), v1.end());
 
-
-
-
-  std::cout << ht.size() << " " << ht.empty() << std::endl;
+  ht.dump(std::cout);
 
   return 0;
 }
