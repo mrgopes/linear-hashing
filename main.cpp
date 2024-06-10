fe10701b4e020f43625038c140058f08473976db
Michal Blava <michal.blava@gmail.com>
2024-06-10 12:44:02 +0200

    eraaase

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include "ADS_set_temp.h"
+#include <random>
+
+using u32    = uint_least32_t;
+using engine = std::mt19937;
 
 int main() {
   std::cout << "Hello, World!" << std::endl;
@@ -10,11 +14,19 @@ int main() {
 
 //  ht.insert(v1.begin(), v1.end());
 
+  std::random_device os_seed;
+  const u32 seed = os_seed();
 
+  engine generator( seed );
+  std::uniform_int_distribution< u32 > distribute( 1, 1000000);
 
-  for (int i {0}; i < 100; ++i) {
-    ht.insert(i);
+  int tracker = 0;
+  for (int i {0}; i < 100000; ++i) {
+    ht.insert(distribute( generator ));
+    tracker++;
   }
+
+  std::cout << tracker << std::endl;
 
   //ht.dump();
 
@@ -30,9 +42,18 @@ int main() {
 
 //  std::cout << *(it++) << std::endl;
 //
+
+  ht.insert(500);
+
+  tracker = 0;
   for (const auto& i : ht) {
-    std::cout << i << std::endl;
+    //std::cout << i << std::endl;
+    tracker++;
   }
+
+  std::cout << tracker << std::endl;
+
+  ht.erase(500);
 
   //int **i {new int*[2147483648]};
 
@@ -40,7 +61,9 @@ int main() {
 
 //  ht.insert(v1.begin(), v1.end());
 
-//  ht.dump(std::cout);
+  //ht.dump(std::cout);
+
+  //std::cout << ht.inhalt[4].find_element(500) << std::endl;
 
   return 0;
 }
